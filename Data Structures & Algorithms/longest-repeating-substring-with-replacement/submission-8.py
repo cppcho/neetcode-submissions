aa8f304ca2d042984f25b225596db9379b8f19bf
Patrick Cho <cppcho@users.noreply.github.com>
2026-08-14 22:38:55 +0900

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,73 @@
+"""
+XYYX k = 1
+XXYX
+XYXX
+
+XYYY  k = 1 
+
+
+- how to know which char to replace
+- where to replace
+- how to calculate the longest length
+- blute force
+    from index 0 X -> rright replace k, skip X -> get max length
+    repeat
+
+
+----
+create set of chars and their first occurance
+
+left, right
+while right < len(s)
+    move right ++ until k budget finish
+    store length
+    update the left
+
+a, b
+AABABBA
+
+left=0,right=0,count=0
+    count = 0
+    res = 1
+    right = 1
+left=0,right=1,count=0
+    count = 0
+    res = 2
+    right = 2
+left=0,right=2,count=0
+    count = 1
+    res = 3
+    right = 3 
+left=0,right=3,count=1
+    res = 4
+left=0,right=4,count=1
+
+
+
+"""
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        st = set(s)
+        for ch in st:
+            left, right = 0, 0
+            count = 0
+            while right < len(s):
+                # inv: left right always valid here
+
+                if s[right] != ch:
+                    if count < k:
+                        count += 1
+                    else:
+                        while s[left] == ch:
+                            left += 1
+                        left += 1
+
+                res = max(res, right-left+1)
+
+                ## move pointer
+                right += 1
+
+        return res
+                
+        
