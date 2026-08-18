108fcf111422dbeb6c0b8bdf3dd69a09873334ba
Patrick Cho <cppcho@users.noreply.github.com>
2026-08-18 21:45:20 +0900

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,41 @@
+"""
+n <= h
+
+1,2,3,4
+
+blute force
+- for each k (1.. max(piles[i]))
+    sum = 0
+    for each i, 
+        sum += (piles[i] // k + 1)
+    if sum > h
+        return k
+
+many  n
+
+k  > n
+
+
+"""
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = len(piles)
+        max_pile = max(piles)
+
+        # how many hours take me if i can eat k bananas per hour
+        def hours_for_k(k):
+            res = 0
+            for pile in piles:
+                res += ((pile-1) // k) + 1
+            return res
+
+        left, right = 1, max_pile
+        while left <= right:
+            mid = (left + right) // 2
+            hours = hours_for_k(mid)
+            if hours <= h:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
